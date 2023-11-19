3fd1f7238c2e55ea4803495180d58fb7c9c52f0d
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-11-19 19:54:08 -0300

    feat(Req03 and Req04): Created Adapter structure and menu item detail activity

---
+++ app/src/main/java/com/example/trybevirtualmenu/adapters/DishAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.trybevirtualmenu.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.trybevirtualmenu.R
+import com.example.trybevirtualmenu.interfaces.Dish
+import com.example.trybevirtualmenu.interfaces.DishItemListener
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.textview.MaterialTextView
+
+class DishAdapter(val dishes: List<Dish>) : Adapter<DishAdapter.DishViewHolder>() {
+    private var dishListener: DishItemListener? = null
+
+    fun setDishListener(listener: DishItemListener) {
+        this.dishListener = listener
+    }
+
+    class DishViewHolder(view: View, dishListener: DishItemListener?) :
+        RecyclerView.ViewHolder(view) {
+        val image: ShapeableImageView = view.findViewById(R.id.item_menu_image)
+        val name: MaterialTextView = view.findViewById(R.id.item_menu_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DishViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_menu_layout, parent, false)
+        return DishViewHolder(view, dishListener)
+    }
+
+    override fun onBindViewHolder(holder: DishViewHolder, position: Int) {
+        holder.name.text = dishes[position].name
+        holder.image.setImageResource(dishes[position].image)
+    }
+
+    override fun getItemCount(): Int = dishes.size
+}
--- app/src/main/java/com/example/trybevirtualmenu/views/MainActivity.kt
+++ app/src/main/java/com/example/trybevirtualmenu/views/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.trybevirtualmenu.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.trybevirtualmenu.R
+import com.example.trybevirtualmenu.adapters.DishAdapter
+import com.example.trybevirtualmenu.models.DishDatabase
 
 class MainActivity : AppCompatActivity() {
+    private val dishesRV: RecyclerView by lazy { findViewById(R.id.main_menu) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dishes = DishDatabase.getDishes()
+        val adapter = DishAdapter(dishes)
+
+        dishesRV.layoutManager = LinearLayoutManager(baseContext)
+        dishesRV.adapter = adapter
     }
 }
